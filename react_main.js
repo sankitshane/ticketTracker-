e653d57fce099a0a0ea99d5aecd2ce332aa67dc9
sankit acharya <sankit.acharya@gmail.com>
2018-02-19 20:46:53 +0530

    fect: Added submit and form in the page

--- react_main.js
+++ react_main.js
@@ -4,15 +4,35 @@ import ReactDOM from 'react-dom';
 class Dash extends React.Component {
   constructor(props){
     super(props);
-    this.state = {}
+    this.state = {"clicked":false}
   }
+
+  handleClick: function () {
+      if this.state.clicked {
+        this.setstate({"clicked":false});
+      }
+      else {
+        this.setstate({"clicked":true});
+      }
+  }
+
   render(){
     return(
       <from className="dash_con">
         <div className="container">
           <h2>Ticket Number</h2>
-            <input name="number" type="text" className="effect"/>
-            <span className="focus-border"></span>
+          <input name="number" type="text" className="effect"/>
+          <span className="focus-border"></span>
+        </div>
+        <div className="container">
+          <div class="squaredThree">
+            <input type="checkbox" value="None" id="squaredThree" name="check"/>
+            <label for="squaredThree"></label>
+          </div>
+          <h3>Pager</h3>
+        </div>
+        <div className="submit">
+          <button id="button" className={this.state.clicked} onClick={this.handleClick}></button>
         </div>
       </from>
     );
